a18a407156bb0cbde65fade4577b503e956081c6
Daniel Díaz <daniel.diaz@linaro.org>
2019-12-12 08:17:07 -0600

    run: Get ARCH from Jenkins environment
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- run.sh
+++ run.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-ARCH=arm64
-if [ $# -gt 1 ]; then
-  ARCH=$1
-fi
-export ARCH
-
 set -a
 . "$(dirname "$(readlink -e "$0")")/jenkins-parameters-741.sh"
 set +a
+
+if [ $# -gt 1 ]; then
+  ARCH=$1
+fi
+
 mkdir -p "${WORKSPACE}"
 ./jenkins-script.sh
