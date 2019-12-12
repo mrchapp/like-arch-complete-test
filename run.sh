77f14a19bf69135bba54024b7a5ac2cf0f1bc1d4
Daniel Díaz <daniel.diaz@linaro.org>
2019-12-12 08:45:49 -0600

    run: Use Jenkins parameters file provided by first argument
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- run.sh
+++ run.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 
-set -a
-. "$(dirname "$(readlink -e "$0")")/jenkins-parameters-822.sh"
-set +a
-
-if [ $# -gt 1 ]; then
-  ARCH=$1
+JENKINS_PARAMETERS_FILE="${1:-$(dirname "$(readlink -e "$0")")/jenkins-parameters-822.sh}"
+if [ -e "${JENKINS_PARAMETERS_FILE}" ]; then
+  set -a
+  . "${JENKINS_PARAMETERS_FILE}"
+  set +a
+else
+  echo "Jenkins parameters file (${JENKINS_PARAMETERS_FILE}) does not exist"
+  exit 1
 fi
 
+if [ ! -v WORKSPACE ]; then
+  echo "WORKSPACE is undefined. Please set an empty WORKSPACE directory."
+  exit 1
+fi
 mkdir -p "${WORKSPACE}"
 ./jenkins-script.sh
