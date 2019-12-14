aae9a8f25ac7843795e2937fdafbd1c8df91ae37
Daniel Díaz <daniel.diaz@linaro.org>
2019-12-13 23:11:15 -0600

    Install requests via pip
    Signed-off-by: Daniel Díaz <daniel.diaz@linaro.org>

--- jenkins-script.sh
+++ jenkins-script.sh
@@ -11,7 +11,7 @@ echo "git describe: [$GIT_DESCRIBE]"
 
 if [[ -v HUDSON_COOKIE ]] || [[ -v CI ]]; then
   SUDO=$(which sudo ||:)
-  ${SUDO} pip3 install jinja2-cli ruamel.yaml
+  ${SUDO} pip3 install jinja2-cli requests ruamel.yaml
   DRY_RUN=""
 else
   DRY_RUN="--dry-run"
